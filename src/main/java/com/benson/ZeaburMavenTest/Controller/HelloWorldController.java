e1098d5c697ca9f44d94c5d88fba7b8565109ccf
derrick <iga00257@gmail.com>
2023-05-01 12:08:43 +0800

    todolist

--- src/main/java/com/benson/ZeaburMavenTest/Controller/HelloWorldController.java
+++ src/main/java/com/benson/ZeaburMavenTest/Controller/HelloWorldController.java
@@ -1,12 +1,44 @@
 package com.benson.ZeaburMavenTest.Controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-public class HelloWorldController {
+@RequestMapping("/todolist")
+public class TodoListController {
+
+    private List<String> todos = new ArrayList<>();
+
     @GetMapping("/")
-    public String index(){
-        return "Hello Zeabur Maven Project!";
+    public ResponseEntity<List<String>> getAllTodos() {
+        return new ResponseEntity<>(todos, HttpStatus.OK);
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> addTodo(@RequestBody String todo) {
+        todos.add(todo);
+        return new ResponseEntity<>("{\"message\": \"Todo added successfully\"}", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{index}")
+    public ResponseEntity<String> deleteTodo(@PathVariable int index) {
+        if (index >= todos.size()) {
+            return new ResponseEntity<>("{\"message\": \"Todo not found\"}", HttpStatus.NOT_FOUND);
+        }
+        todos.remove(index);
+        return new ResponseEntity<>("{\"message\": \"Todo deleted successfully\"}", HttpStatus.OK);
+    }
+
+    @PutMapping("/{index}")
+    public ResponseEntity<String> updateTodo(@PathVariable int index, @RequestBody String todo) {
+        if (index >= todos.size()) {
+            return new ResponseEntity<>("{\"message\": \"Todo not found\"}", HttpStatus.NOT_FOUND);
+        }
+        todos.set(index, todo);
+        return new ResponseEntity<>("{\"message\": \"Todo updated successfully\"}", HttpStatus.OK);
     }
 }
